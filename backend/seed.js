133a73cb5cfd9e74def95115f65b4ab4ccf5e4f7
Messaoud ZOUGGAB <messaoud.zouggab@gmail.com>
2024-01-31 11:41:54 +0100

    US1_creationDB done

--- backend/seed.js
+++ backend/seed.js
@@ -4,7 +4,7 @@
 require("dotenv").config();
 
 // Import Faker library for generating fake data
-const { faker } = require("@faker-js/faker");
+// const { faker } = require("@faker-js/faker");
 
 // Import database client
 const database = require("./database/client");
@@ -18,18 +18,45 @@ const seed = async () => {
     /* ************************************************************************* */
 
     // Generating Seed Data
+    await database.query("delete from item");
+    queries.push(
+      database.query(
+        `INSERT INTO item (brand, model, name, color, date_release) VALUES
+            ('Air Jordan', '1', 'Chicago', 'WHITE/BLACK-RED', '16/09/1985'),
+            ('Nike', 'Cortez', 'Forest Gump', 'WHITE/VARSITY RED-VARSITY ROYAL', '28/02/2015'),
+            ('Onitsuka Tiger', 'Mexico 66', 'Kill Bill', 'YELLOW/BLACK', '01/01/2017'),
+            ('Nike', 'Air Yeezy 2', 'Red October', 'RED', '09/02/2014'),
+            ('Adidas', 'Stan Smith', 'White Green', 'RUNNING WHITE/RUNNING WHITE/FAIRWAY', '1964'),
+            ('Adidas', 'Yeezy Boost 350', 'Turtledove', 'TURTLEDOVE/BLUE GRAPHITE/CHALK WHITE', '27/06/2015'),
+            ('Air Jordan', '1', 'Maison Chateau Rouge', 'PALE VANILLA/CINNAMON', '30/11/2019')`
+      )
+    );
 
+    await database.query("delete from category");
+    queries.push(
+      database.query(
+        `INSERT INTO category (name) VALUES
+            ('Running'),
+            ('Basketball'),
+            ('Training'),
+            ('Skate'),
+            ('Trail'),
+            ('Fashion'),
+            ('Retro'),
+            ('Collab')`
+      )
+    );
     // Optional: Truncate tables (remove existing data)
-    await database.query("truncate item");
+    // await database.query("truncate item");
 
     // Insert fake data into the 'item' table
-    for (let i = 0; i < 10; i += 1) {
-      queries.push(
-        database.query("insert into item(title) values (?)", [
-          faker.lorem.word(),
-        ])
-      );
-    }
+    // for (let i = 0; i < 10; i += 1) {
+    //   queries.push(
+    //     database.query("insert into item(title) values (?)", [
+    //       faker.lorem.word(),
+    //     ])
+    //   );
+    // }
 
     /* ************************************************************************* */
 
